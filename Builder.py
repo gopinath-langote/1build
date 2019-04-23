cff6069916042ba9629bcabb53bd8a142ad6a287
Gopinath Langote <gopinathlangote11@gmail.com>
2019-04-23 20:19:33 +0200

    Add builder class

---
+++ Builder.py
@@ -0,0 +1,24 @@
+import yaml
+import sys
+
+
+class Command:
+    def __init__(self, cmd, description):
+        self.cmd = cmd
+        self.description = description
+
+
+if __name__ == '__main__':
+    commandToExecute = sys.argv[1]
+    with open("1build.yaml", 'r') as stream:
+        try:
+            content = yaml.safe_load(stream)
+            project = content["project"]
+            commands = []
+            for cmd in content["commands"]:
+                command = cmd.get(commandToExecute)
+                if command is not None:
+                    print "executing " + command["description"]
+                    print command["cmd"]
+        except yaml.YAMLError as exc:
+            print(exc)
