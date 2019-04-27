ec9ac90fb48baba0717cc47fae57a0f85d969eb0
Gopinath Langote <gopinathlangote11@gmail.com>
2019-04-27 15:49:39 +0200

    ⎘ use of ruamel.yam Issue: #13

--- setup.py
+++ setup.py
@@ -3,20 +3,34 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name='gopitest',
+    name='1build',
     version='0.0.1',
     scripts=['1build'],
     author="Gopinath Langote",
-    install_requires=['PyYAML>=3.12'],
+    install_requires=[
+        'ruamel.yaml>=0.15.94'
+    ],
     author_email="gopinathlangote11@gmail.com",
-    description="1build utility",
+    description="Unified build tool for all project needs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gopinath-langote/1build",
     packages=[],
     classifiers=[
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Build Tools",
+        "Intended Audience :: Developers",
         "Operating System :: OS Independent",
     ],
 )
