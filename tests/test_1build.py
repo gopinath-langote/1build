1e89b39c0d18614ea3f5238e304ae305cb651644
Gopinath Langote <gopinathlangote11@gmail.com>
2019-04-27 18:44:01 +0200

    🔨Make test runnable from main folder Issue: #16

--- tests/test_1build.py
+++ tests/test_1build.py
@@ -3,11 +3,11 @@
 import pytest
 import imp
 
-build = imp.load_source('1build', '../1build')
+build = imp.load_source('1build', '1build')
 
 
 def test_build_successful_command(capsys):
-    build.run("build_file.yaml", ['file_name', 'build'])
+    build.run("tests/build_file.yaml", ['file_name', 'build'])
     captured = capsys.readouterr()
     expected_message = "---------------------------------------------------\n" \
                        "Name: build\n" \
