ce853105f814dbc7095aec36229f9017fec53873
Gopinath Langote <gopinathlangote11@gmail.com>
2019-05-01 19:20:21 +0200

    🔨 add test for invalid yaml file Issue: #16

--- tests/test_1build.py
+++ tests/test_1build.py
@@ -14,3 +14,21 @@ def test_build_successful_command(capsys):
                        "Command: ls\n" \
                        "---------------------------------------------------\n"
     assert expected_message in captured.out
+
+
+def test_should_fail_with_invalid_file_message_if_file_is_not_in_correct_yaml_format(capsys):
+    build.run("tests/invalid_yaml_file.yaml", ['file_name', 'build'])
+    captured = capsys.readouterr()
+
+    invalid_file_error_message = "Error in parsing 'tests/invalid_yaml_file.yaml' config file. Make sure file is in correct format.\n" \
+                                 "Sample format is: \n\n" + \
+                                 "---------------------------------------------------\n" \
+                                 "project: JUnit5" + "\n" + \
+                                 "commands:" + "\n" + \
+                                 "  - build: ./gradlew clean build" + "\n" + \
+                                 "  - lint: ./gradlew spotlessApply\n" \
+                                 "---------------------------------------------------\n"
+
+    assert invalid_file_error_message in captured.out
+
+
