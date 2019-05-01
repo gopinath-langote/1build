a07354fe4995e7ba1da7c4095ed7320acca024be
Gopinath Langote <gopinathlangote11@gmail.com>
2019-05-01 19:20:21 +0200

    🔨 add help command Issue: #16

--- tests/test_1build.py
+++ tests/test_1build.py
@@ -21,14 +21,25 @@ def test_should_fail_with_invalid_file_message_if_file_is_not_in_correct_yaml_fo
     captured = capsys.readouterr()
 
     invalid_file_error_message = "Error in parsing 'tests/invalid_yaml_file.yaml' config file. Make sure file is in correct format.\n" \
-                                 "Sample format is: \n\n" + \
+                                 "Sample format is:\n\n" \
                                  "---------------------------------------------------\n" \
-                                 "project: JUnit5" + "\n" + \
-                                 "commands:" + "\n" + \
-                                 "  - build: ./gradlew clean build" + "\n" + \
+                                 "project: Sample Project\n" \
+                                 "commands:\n" \
+                                 "  - build: ./gradlew clean build\n" \
                                  "  - lint: ./gradlew spotlessApply\n" \
-                                 "---------------------------------------------------\n"
+                                 "---------------------------------------------------"
 
     assert invalid_file_error_message in captured.out
 
 
+def test_should_print_help_on_help_command(capsys):
+    build.run("tests/build_file.yaml", ['file_name', 'help'])
+    captured = capsys.readouterr()
+
+    invalid_file_error_message = "Usage: 1build <command_name> \n\n" \
+                                 "project: Sample Project\n" + \
+                                 "commands:\n" + \
+                                 "build | ls\n" + \
+                                 "lint | ls"
+
+    assert invalid_file_error_message in captured.out
