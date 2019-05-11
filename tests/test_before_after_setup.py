425abd5b7dce0244686d67f08e7dcad047720e5f
Manish Jain <jaiin.maniish@gmail.com>
2019-05-11 20:15:52 +0200

    Separate test files for before and after command. (#43)

---
+++ tests/test_before_after_setup.py
@@ -0,0 +1,52 @@
+import imp
+
+build = imp.load_source('1build', '1build')
+from test_utils import dash
+
+
+def test_build_successful_with_before_and_after_command(capsys):
+    build.run("tests/data/build_file_with_before_and_after.yaml", ['file_name', 'build'])
+    captured = capsys.readouterr()
+    expected_cmd_message = "" + dash + "\n" \
+                                       "Name: build\n" \
+                                       "Command: echo 'Running build'\n" \
+                                       "Before: echo 'Running some setup script'\n" \
+                                       "After: echo 'Running some cleanup script'\n" \
+                           + dash + "\n"
+    expected_before_command_output = "Running some setup script"
+    expected_command_output = "Running build"
+    expected_after_command_output = "Running some cleanup script"
+    assert expected_cmd_message in captured.out
+    assert expected_before_command_output in captured.out
+    assert expected_command_output in captured.out
+    assert expected_after_command_output in captured.out
+
+
+def test_should_work_with_only_before_command(capsys):
+    build.run("tests/data/build_file_with_before_only.yaml", ['file_name', 'build'])
+    captured = capsys.readouterr()
+    expected_cmd_message = "" + dash + "\n" \
+                                       "Name: build\n" \
+                                       "Command: echo 'Running build'\n" \
+                                       "Before: echo 'Running some setup script'\n" \
+                           + dash + "\n"
+    expected_before_command_output = "Running some setup script"
+    expected_command_output = "Running build"
+    assert expected_cmd_message in captured.out
+    assert expected_before_command_output in captured.out
+    assert expected_command_output in captured.out
+
+
+def test_should_work_with_only_after_command(capsys):
+    build.run("tests/data/build_file_with_after_only.yaml", ['file_name', 'build'])
+    captured = capsys.readouterr()
+    expected_cmd_message = "" + dash + "\n" \
+                                       "Name: build\n" \
+                                       "Command: echo 'Running build'\n" \
+                                       "After: echo 'Running some cleanup script'\n" \
+                           + dash + "\n"
+    expected_command_output = "Running build"
+    expected_after_command_output = "Running some cleanup script"
+    assert expected_cmd_message in captured.out
+    assert expected_command_output in captured.out
+    assert expected_after_command_output in captured.out
