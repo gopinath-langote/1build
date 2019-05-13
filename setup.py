9cb60828844c4b93e55de3465c999286b3cbcbfe
Gopinath Langote <gopinathlangote11@gmail.com>
2019-05-13 21:55:33 +0200

    Update package version in setup.py Issue: #10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='1build',
-    version='0.0.2',
+    version='0.0.3',
     scripts=['1build'],
     license="MIT License",
     author="Gopinath Langote",
