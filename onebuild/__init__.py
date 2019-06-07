3f4cdede6fc089ca84776afc7e84bef619ef758c
Mayur Kumbhar <mayur.kumbhar@outlook.com>
2019-06-07 23:35:14 +0530

    Show version of `1build` (#80)

--- onebuild/__init__.py
+++ onebuild/__init__.py
@@ -3,3 +3,9 @@
 """
 
 __author__ = 'Gopinath Langote'
+
+# New release version will be updated here
+# e.g. 1)for dev ->       __version__ = '1.0.dev0'
+#      2)for release ->   __version__ = '1.0.1'
+# this will automatically update setup.py version to latest release version.
+__version__ = '1.0.0'
