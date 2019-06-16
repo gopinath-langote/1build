4ef0e3aec9488bcb83e0e3391e0e2c9746155f79
in-rmoitra <rmoitra@thoughtworks.com>
2019-06-17 00:03:04 +0530

    Release 1.0.0 Issue: #84 Import abc as abstract_class for ease of readability

--- onebuild/commands/command.py
+++ onebuild/commands/command.py
@@ -1,8 +1,8 @@
-import abc
+import abc as abstract_class
 
 
-class Command(metaclass=abc.ABCMeta):
+class Command(metaclass=abstract_class.ABCMeta):
 
-    @abc.abstractmethod
+    @abstract_class.abstractmethod
     def execute(self, arg_parser, arguments, build_file_name, command_name):
         pass
