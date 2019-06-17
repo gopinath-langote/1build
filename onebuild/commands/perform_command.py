40a9416d63696a49e2a7d3a6517de8fc19af68a4
in-rmoitra <rmoitra@thoughtworks.com>
2019-06-17 20:31:58 +0530

    Release 1.1.0 Issue: #84 Inline executor to PerformCommand.execute

--- onebuild/commands/perform_command.py
+++ onebuild/commands/perform_command.py
@@ -1,6 +1,8 @@
+import os
+
 from onebuild.commands.command import Command
 from onebuild.config_parser import parse_project_config
-from onebuild.executor import execute
+from onebuild.utils import DASH
 
 
 class PerformCommand(Command):
@@ -8,4 +10,19 @@ class PerformCommand(Command):
     def execute(self, arg_parser, arguments, build_file_name, command_name):
         project = parse_project_config(build_file_name)
         command = project.get_command(command_name)
-        execute(command, project.before, project.after)
+
+        cmd = command.cmd
+        print(DASH + "\nName: " + command.name + "\nCommand: " + command.cmd)
+
+        before = project.before
+        after = project.after
+
+        if before:
+            print("Before: " + before)
+            cmd = before + " && " + cmd
+        if after:
+            print("After: " + after)
+            cmd = cmd + " && " + after
+            
+        print(DASH)
+        os.system(cmd)
