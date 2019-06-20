780d19eedb0e2817b9bc430982ae5fe3ae5138ff
in-rmoitra <rmoitra@thoughtworks.com>
2019-06-20 20:46:58 +0530

    Handle index out of error exception in init command with proper message to the user

--- onebuild/init_command.py
+++ onebuild/init_command.py
@@ -11,4 +11,14 @@ def default_yaml_file(project_name):
 class InitCommand(Command):
 
     def execute(self, arg_parser, arguments, build_file_name, command_name):
-        write("1build.yaml", "w", default_yaml_file(arguments[1]))
+        try:
+            write("1build.yaml", "w", default_yaml_file(arguments[1]))
+        except IndexError:
+            raise ValueError(
+                __project_name_not_found_error_message__()
+            )
+
+
+def __project_name_not_found_error_message__():
+    return "Please add project name after init command as " \
+           "follows - 1build --init myproject"
--- tests/test_init_command.py
+++ tests/test_init_command.py
@@ -16,3 +16,11 @@ def test_create_default_yaml_file():
     mock_file_writer.assert_called_with(
         '1build.yaml', 'w',
         "project: some project\ncommands:\n  - build: echo 'Running build'")
+
+
+def test_error_message_if_file_name_is_not_provided_with_init(capsys):
+    run("", ['--init'])
+    captured = capsys.readouterr()
+    expected_message = "Please add project name after init command as " \
+                       "follows - 1build --init myproject"
+    assert expected_message in captured.out
