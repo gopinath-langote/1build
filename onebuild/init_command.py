bc96a6c800497883c71c30715cf78a1f15e08957
Gopinath Langote <gopinathlangote11@gmail.com>
2019-06-21 14:48:30 +0200

    Do not override file if exists for init command Issue: #103

--- onebuild/init_command.py
+++ onebuild/init_command.py
@@ -1,3 +1,5 @@
+import os
+
 from onebuild.command import Command
 from onebuild.file_writer import write
 
@@ -12,7 +14,9 @@ class InitCommand(Command):
 
     def execute(self, arg_parser, arguments, build_file_name, command_name):
         if len(arguments) < 2:
-            raise ValueError(__project_name_not_found_error_message__())
+            print(__project_name_not_found_error_message__())
+        elif os.path.isfile("1build.yaml"):
+            print(__file_already_exists_message__())
         else:
             write("1build.yaml", "w", default_yaml_file(arguments[1]))
 
@@ -20,3 +24,7 @@ class InitCommand(Command):
 def __project_name_not_found_error_message__():
     return "The 'project name' parameter is missing with --init" \
            "\n\nusage: 1build --init project_name"
+
+
+def __file_already_exists_message__():
+    return "1build.yaml configuration file already exists."
