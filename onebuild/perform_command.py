942e5986a3578ca27bbe35be8ea03003721a938a
Gopinath Langote <gopinathlangote11@gmail.com>
2019-06-29 13:30:38 +0200

    Execute multiple 1build command at once Issue: #102

--- onebuild/perform_command.py
+++ onebuild/perform_command.py
@@ -9,10 +9,17 @@ class PerformCommand(Command):
 
     def execute(self, arg_parser, arguments, build_file_name, command_name):
         project = parse_project_config(build_file_name)
+        args = arg_parser.parse_args(args=arguments)
+        for cmd in args.command:
+            self.__execute_command__(project, cmd)
+
+    @staticmethod
+    def __execute_command__(project, command_name):
         command = project.get_command(command_name)
 
         cmd = command.cmd
-        print(DASH + "\nName: " + command.name + "\nCommand: " + command.cmd)
+        print(DASH + "\nName: " + command.name +
+              "\nCommand: " + command.cmd)
 
         before = project.before
         after = project.after
